e17cba4ddf5be35566fdaac545877f7f4dafe03b
munizin <pmdm.sys@gmail.com>
2022-12-31 02:01:21 -0300

    First Commit

---
+++ XablauGod/Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  XablauGod
+//
+//  Created by Philippe Muniz Gomes on 31/12/22.
+//
+
+import Foundation
+
+public class Utils {
+
+    public func sayHello(what: String) {
+        print("Xablau's Dinasty says: ", what)
+    }
+}
